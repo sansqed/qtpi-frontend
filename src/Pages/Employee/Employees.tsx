342c953accaddcf04ce223cb5377961a420f421c
sneedle <69748956+Kaelefor@users.noreply.github.com>
2023-03-09 02:33:48 +0800

    almost done with employees page, need smoll button

--- src/Pages/Employee/Employees.tsx
+++ src/Pages/Employee/Employees.tsx
@@ -5,7 +5,14 @@ import "./Employees.css"
 const Employees: React.FC = () => {
     return (
         <div className="employees-container">
-            <Sidebar/>
+            <div className="employees-content-wrapper">
+                <Sidebar/>
+                <div className="employee-list-container">
+                    <div className="employee-list-header">
+                        <h1>EMPLOYEE LIST</h1>
+                    </div>
+                </div>
+            </div>
         </div>      
     );
 };
