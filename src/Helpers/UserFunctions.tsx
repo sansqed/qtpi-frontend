5048e301b8375292f28e0dfaad132cead898af0f
sansqed <44792e@gmail.com>
2023-05-03 01:07:18 +0800

    startup error fix

--- src/Helpers/UserFunctions.tsx
+++ src/Helpers/UserFunctions.tsx
@@ -1,36 +1,23 @@
 export const isLoggedIn = () => {
 
-    // try {(localStorage.getItem("token"))
-    //     return true
-    // }catch(error) {
-    //     return false
-    // }
-    if (localStorage.getItem("token") === null)
+    try {
+        if (localStorage.getItem("token") === null)
+            return false
+        else
+            return true
+    }catch(error) {
         return false
-    else
-        return true
+    }
+    
         
     // localStorage.getItem("token")!==null? true:false
     // return true
 }
 
 export const getToken = () => {
-    try {(localStorage.getItem("token"))
-        return JSON.parse(localStorage.getItem("token") ||"")
-    }catch(error) {
-        return false
-    }
-        
-    // return localStorage.getItem("token") !== undefined?JSON.parse(localStorage.getItem("token") ||""):null
-    // return true
+    return JSON.parse(localStorage.getItem("token") ||"")
 }
 
 export const getRequester = () => {
-    try {(localStorage.getItem("requester"))
-        return localStorage.getItem("requester")
-    }catch(error) {
-        return false
-    }
-    // return localStorage.getItem("requester") !== undefined?JSON.parse(localStorage.getItem("requester") ||""):null
-    // // return true
+    return localStorage.getItem("requester")
 }
