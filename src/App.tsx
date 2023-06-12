3530db646d5cbfa677a89c63e4e030c981a2f788
sycasec <kadelcastillo@up.edu.ph>
2023-06-12 18:40:23 +0800

    finished payroll export to pdf
    it now works!

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { IsLoggedIn } from './Helpers/UserFunctions';
 
 import Login from './Pages/Login/Login';
@@ -15,58 +15,58 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 import { CheckUserSession } from './ApiCalls/AuthApi';
 import toasterConfig, { tokenExpiryConfig } from './Helpers/ToasterConfig';
 
-function App() {  
-  library .add(fas)
-  const [loggedIn, setloggedIn]  = useState(true)
+function App() {
+  library.add(fas)
+  const [loggedIn, setloggedIn] = useState(true)
 
   IsLoggedIn().
-    then((response)=>{
+    then((response) => {
       setloggedIn(response)
     })
 
   return (
     <div className="App">
       <div className='toaster-container'>
-        <Toaster position="bottom-center" reverseOrder={false}/>
+        <Toaster position="bottom-center" reverseOrder={false} />
       </div>
       <BrowserRouter>
         <Routes>
-          <Route 
-              path="/" 
-              element={1? <Dashboard/>:<Login/>}  
-            />
+          <Route
+            path="/"
+            element={1 ? <Dashboard /> : <Login />}
+          />
           <Route
             path="/payroll"
-            element={loggedIn? <Payroll/>:<Login/>}
+            element={loggedIn ? <Payroll /> : <Login />}
           />
           <Route
             path="/expenses"
-            element={loggedIn? <Expenses/>:<Login/>}
+            element={loggedIn ? <Expenses /> : <Login />}
           />
           <Route
             path="/employees"
-            element={loggedIn? <Employees/>:<Login/>}
+            element={loggedIn ? <Employees /> : <Login />}
           />
           <Route
             path="/employees/:action"
-            element={loggedIn? <Employees/>:<Login/>}
+            element={loggedIn ? <Employees /> : <Login />}
           />
           <Route
             path="/employees/:action/:user_id"
-            element={loggedIn? <Employees/>:<Login/>}
+            element={loggedIn ? <Employees /> : <Login />}
           />
           <Route
             path="/users"
-            element={loggedIn? <Users/>:<Login/>}
-          /> 
+            element={loggedIn ? <Users /> : <Login />}
+          />
           <Route
             path="/users/:action"
-            element={loggedIn? <Users/>:<Login/>}
-          /> 
+            element={loggedIn ? <Users /> : <Login />}
+          />
           <Route
             path="/users/:action/:user_id"
-            element={loggedIn? <Users/>:<Login/>}
-          /> 
+            element={loggedIn ? <Users /> : <Login />}
+          />
         </Routes>
         {/* <TokenListener/> */}
       </BrowserRouter>
---
+++ src/Types/PayrollDetail.tsx
@@ -0,0 +1,12 @@
+type PayrollDetailType = {
+  full_name: string;
+  position: string;
+  num_days: number;
+  rate: string;
+  gross_salary: number;
+  cash_advance: number;
+  employee_sss: string;
+  net_salary: number;
+};
+
+export default PayrollDetailType;
